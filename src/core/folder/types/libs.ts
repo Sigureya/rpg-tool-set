b87e733be3053573879c3505f353b86ac750271e
Sigureya <sigureya1208@gmail.com>
2025-01-11 03:48:13 +0900

    ライブラリの内、使う関数をまとめた

--- src/core/folder/types/libs.ts
+++ src/core/folder/types/libs.ts
@@ -1,6 +1,11 @@
 import type fs from "node:fs/promises";
 import type path from "node:path";
 export interface Libs {
-  fileSystem: typeof fs;
-  path: typeof path;
+  fileSystem: FsLib;
+  path: PathLib;
 }
+export type PathLib = Pick<typeof path, "resolve" | "basename">;
+export type FsLib = Pick<
+  typeof fs,
+  "mkdir" | "access" | "readFile" | "writeFile" | "readdir"
+>;
