d9efb0b851b2e07f3719a0e4e84b6e67a317669d
Sigureya <sigureya1208@gmail.com>
2025-01-12 04:25:39 +0900

    処理を小さくまとめた。古いファイルは削除

--- src/core/folder/jsonFolderTraits.ts
+++ src/core/folder/jsonFolderTraits.ts
@@ -1,9 +1,9 @@
-import {
-  toData,
-  type FileDataType,
-  type FolderTraits,
-  type JSONFolderTraits,
-} from "./types";
+import type { FileDataType, FolderTraits } from "./types";
+
+export interface JSONFolderTraits<T> {
+  makeDefaultObject: () => T;
+  readJSON: (jsonText: string, buffer: Buffer) => T;
+}
 
 export class JSONFolderTraitsClass<T> implements FolderTraits<T> {
   private _traits: JSONFolderTraits<T>;
@@ -14,12 +14,13 @@ export class JSONFolderTraitsClass<T> implements FolderTraits<T> {
     return `.json` as const;
   }
   makeDefault(): FileDataType {
-    return toData(this._traits.makeDefaultObject());
+    return this.toFileData(this._traits.makeDefaultObject());
   }
   async readFile(buffer: Buffer): Promise<T> {
     return this._traits.readJSON(buffer.toString("utf-8"), buffer);
   }
   toFileData(data: T): FileDataType {
-    return toData(data);
+    const json: string = JSON.stringify(data);
+    return Buffer.from(json);
   }
 }
--- src/core/folder/types/index.ts
+++ src/core/folder/types/index.ts
@@ -1,4 +1,3 @@
 export * from "./filedata";
 export * from "./trait";
 export * from "./libs";
-export * from "./folderTraitJSON";
