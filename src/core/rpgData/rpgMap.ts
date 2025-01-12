cc9404d6531f9a8e255784cf7e79a10f103ceb2c
Sigureya <sigureya1208@gmail.com>
2025-01-13 03:56:44 +0900

    マップ読み込みに使う中間データ

--- src/core/rpgData/rpgMap.ts
+++ src/core/rpgData/rpgMap.ts
@@ -3,11 +3,34 @@ import type { Data_Map } from "@sigureya/rpgtypes";
 export type MapFileName = `Map${string}`;
 
 export interface MapFileInfo {
+  filename: MapFileName;
+  editingName: string;
+  id: number;
+}
+
+export interface MapFilePromise {
+  data: Promise<Data_Map>;
+  filename: MapFileName;
+  editingName: string;
+  id: number;
+}
+
+export interface MapFileData extends MapFileInfo {
   data: Data_Map;
   filename: MapFileName;
   editingName: string;
   id: number;
 }
+
+export const resolveMapFile = async (
+  map: MapFilePromise
+): Promise<MapFileData> => ({
+  filename: map.filename,
+  editingName: map.editingName,
+  id: map.id,
+  data: await map.data,
+});
+
 export const createMapFileName = (mapId: number): MapFileName => {
   return `Map${mapId.toString().padStart(3, "0")}` as const;
 };
@@ -16,14 +39,15 @@ export const isMapFileName = (filename: string): filename is MapFileName => {
   return /^Map\d{3}$/.test(filename);
 };
 
+export const validateMapData = (map: Data_Map) => true;
+
 export const createMapFileInfo = (
   mapId: number,
-  data: Data_Map
+  editingName = ""
 ): MapFileInfo => {
   return {
-    data,
     filename: createMapFileName(mapId),
-    editingName: data.displayName,
+    editingName,
     id: mapId,
   };
 };
