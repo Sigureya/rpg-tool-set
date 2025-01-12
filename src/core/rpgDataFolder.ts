4b5d01ee200277135e1d77782097cbb8eb34e3ce
Sigureya <sigureya1208@gmail.com>
2025-01-13 02:27:32 +0900

    既存クラスの継承で作ることにした。データ型はstring扱いにし、個別の型で読みたい場合は専用関数を使うことにする

--- src/core/rpgDataFolder.ts
+++ src/core/rpgDataFolder.ts
@@ -1,17 +1,22 @@
-import type { FileFolderInterFace } from "./folder";
-import type { AnyDataType, DataTypesTable } from "@sigureya/rpgtypes";
-import { FileFolder, type Libs } from "./folder";
+import type { Libs } from "./folder";
+import { FileFolder, type FileFolderInterFace } from "./folder";
+import type { DataTypesTable, IdentifiedItems } from "@sigureya/rpgtypes";
+import { filterValidData } from "./utilst";
+import type { MapFileInfo } from "./rpgData/rpgMap";
 
-export class RpgDataFolder implements FileFolderInterFace {
-  get ext() {
-    return ".json" as const;
+export class RpgDataFolder extends FileFolder<string> {
+  constructor(libs: Libs, basePath: string) {
+    super(libs, basePath, {
+      ext: ".json",
+      readFile: async (data) => data.toString("utf-8"),
+      toFileData: (data) => Buffer.from(JSON.stringify(data)),
+      makeDefault: (filename) => {
+        // TODO:ファイル名に応じて良い感じに処理を行う
+        throw new Error("Method not implemented.");
+      },
+    });
   }
-  mkDir(): Promise<void> {
-    throw new Error("Method not implemented.");
-  }
-  read(filename: string): Promise<unknown> {
-    throw new Error("Method not implemented.");
-  }
+
   write(filename: string, data: unknown): Promise<void> {
     const safeFileName = this.safeFilename(filename);
     if (!safeFileName) {
@@ -20,6 +25,18 @@ export class RpgDataFolder implements FileFolderInterFace {
 
     throw new Error("Method not implemented.");
   }
+  async readJSON<FileName extends keyof DataTypesTable>(
+    filename: FileName
+  ): Promise<DataTypesTable[FileName][]> {
+    const text = await this.read(filename);
+
+    return [];
+  }
+  writeJSON<FileName extends keyof DataTypesTable>(
+    data: DataTypesTable[FileName],
+    filename: FileName
+  ) {}
+
   safeFilename(filename: string): keyof DataTypesTable | undefined {
     return this.filenames().find((name) => name === filename);
   }
@@ -38,5 +55,8 @@ export class RpgDataFolder implements FileFolderInterFace {
       "CommonEvents",
       "MapInfos",
     ] as const;
+  }
+  readMapData(mapId: number): Promise<MapFileInfo> {
+    throw new Error("Method not implemented.");
   }
 }
