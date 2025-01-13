4a7b8a672f56e43d28ca0493a2ea313f0f3a2d5b
Sigureya <sigureya1208@gmail.com>
2025-01-14 04:58:26 +0900

    データ生成関数を追加

--- src/mock/libs/rpgMakerMZ/createData/mainData.ts
+++ src/mock/libs/rpgMakerMZ/createData/mainData.ts
@@ -10,7 +10,9 @@ import type {
   ParamArray,
 } from "@sigureya/rpgtypes";
 
-export const createParamArray = (): ParamArray => [0, 0, 0, 0, 0, 0, 0, 0];
+export const createParamArray = (): ParamArray => {
+  return [0, 0, 0, 0, 0, 0, 0, 0];
+};
 
 export const createActor = (proto?: Partial<Data_Actor>): Data_Actor => {
   return {
@@ -57,9 +59,9 @@ export const createArmor = (proto?: Partial<Data_Armor>): Data_Armor => {
     id: 0,
     name: "",
     note: "",
-    //    iconIndex: 0,
+    iconIndex: 0,
     description: "",
-    //    etypeId: 0,
+    etypeId: 0,
     params: createParamArray(),
     traits: [],
 
@@ -72,11 +74,13 @@ export const createWeapon = (proto?: Partial<Data_Weapon>): Data_Weapon => {
     id: 0,
     name: "",
     note: "",
-    //    iconIndex: 0,
+    iconIndex: 0,
     description: "",
-    //    etypeId: 0,
+    etypeId: 0,
+    wtypeId: 0,
     params: createParamArray(),
     traits: [],
+    damage: createDamage(),
 
     ...proto,
   };
@@ -85,11 +89,106 @@ export const createWeapon = (proto?: Partial<Data_Weapon>): Data_Weapon => {
 export const createClass = (proto?: Partial<Data_Class>): Data_Class => {
   return {
     id: 0,
-    expParams: [],
-    learnings: [],
     name: "",
     note: "",
     traits: [],
+    learnings: [],
     params: createParamArray(),
+    expParams: [],
+    ...proto,
+  };
+};
+
+export const createDamage = (
+  proto?: Partial<Data_Skill["damage"]>
+): Data_Skill["damage"] => {
+  return {
+    type: 0,
+    elementId: 0,
+    formula: "0",
+    variance: 20,
+    critical: false,
+    ...proto,
+  };
+};
+
+export const createItem = (proto?: Partial<Data_Item>): Data_Item => {
+  return {
+    id: 0,
+    name: "",
+    iconIndex: 0,
+    description: "",
+    note: "",
+    price: 0,
+    consumable: false,
+    scope: 0,
+    occasion: 0,
+    speed: 0,
+    successRate: 100,
+    repeats: 1,
+    tpGain: 0,
+    hitType: 0,
+    animationId: 0,
+    damage: createDamage(),
+    effects: [],
+    ...proto,
+  };
+};
+
+export const createSkill = (proto?: Partial<Data_Skill>): Data_Skill => {
+  return {
+    animationId: 0,
+    damage: createDamage(),
+    description: "",
+    hitType: 0,
+    iconIndex: 0,
+    id: 0,
+    message1: "",
+    message2: "",
+    mpCost: 0,
+    name: "",
+    note: "",
+    occasion: 0,
+    repeats: 1,
+    requiredWtypeId1: 0,
+    requiredWtypeId2: 0,
+    scope: 0,
+    speed: 0,
+    stypeId: 0,
+    successRate: 100,
+    tpCost: 0,
+    tpGain: 0,
+    effects: [],
+    messageType: 0,
+
+    ...proto,
+  };
+};
+
+export const createState = (proto?: Partial<Data_State>): Data_State => {
+  return {
+    id: 0,
+    name: "",
+    iconIndex: 0,
+    restriction: 0,
+    priority: 50,
+    motion: 0,
+    overlay: 0,
+    removeAtBattleEnd: false,
+    removeByRestriction: false,
+    autoRemovalTiming: 0,
+    minTurns: 1,
+    maxTurns: 1,
+    removeByDamage: false,
+    chanceByDamage: 100,
+    removeByWalking: false,
+    stepsToRemove: 100,
+    message1: "",
+    message2: "",
+    message3: "",
+    message4: "",
+    traits: [],
+    note: "",
+    ...proto,
   };
 };
